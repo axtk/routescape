ff42212cd5f0577820fca1968699108e7ddc8ebc
axtk <axtkx@inbox.ru>
2025-05-26 20:01:05 +0300

    lint

--- lib/url/isSameOrigin.ts
+++ lib/url/isSameOrigin.ts
@@ -1,8 +1,7 @@
 export const isSameOrigin = (url: string): boolean => {
     try {
         return (
-            new URL(url, window.location.href).origin ===
-            window.location.origin
+            new URL(url, window.location.href).origin === window.location.origin
         );
     } catch {
         return false;
