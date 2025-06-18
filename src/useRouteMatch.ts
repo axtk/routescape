e3e8099f3fbe489370a29496694098abbeae1f32
axtk <axtkx@inbox.ru>
2025-06-18 23:05:55 +0300

    lint

--- src/useRouteMatch.ts
+++ src/useRouteMatch.ts
@@ -1,6 +1,6 @@
 import {useContext} from 'react';
-import type {LocationPattern} from './types/LocationPattern';
 import {RouteContext} from './RouteContext';
+import type {LocationPattern} from './types/LocationPattern';
 
 export function useRouteMatch<P extends LocationPattern>(locationPattern: P) {
     return useContext(RouteContext)._getMatch<P>(locationPattern);
