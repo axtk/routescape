a951f29a4fc74277b333de771d18fb189d9526ce
axtk <axtkx@inbox.ru>
2025-06-19 10:20:53 +0300

    replace tuple returned from useRoute with object

--- src/useRoute.ts
+++ src/useRoute.ts
@@ -1,18 +1,14 @@
 import {useContext, useEffect, useMemo, useState} from 'react';
 import {RouteContext} from './RouteContext';
-import type {Route} from './utils/Route';
 
-export type WithRoute = Route['evaluate'];
-
-export function useRoute(): [Route, WithRoute] {
+export function useRoute() {
     let route = useContext(RouteContext);
     let [, setHref] = useState(route.href);
 
     useEffect(() => route.subscribe(href => setHref(href)), [route]);
 
-    return useMemo(() => {
-        let withRoute = route.evaluate.bind(route);
-
-        return [route, withRoute];
-    }, [route]);
+    return useMemo(() => ({
+        route,
+        withRoute: route.evaluate.bind(route),
+    }), [route]);
 }
