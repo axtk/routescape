f1fef6a5d84c09722d0870185d43899ee43aea42
axtk <axtkx@inbox.ru>
2025-06-23 18:54:35 +0300

    run useNavigationCallback on mount

--- src/useNavigationStart.ts
+++ src/useNavigationStart.ts
@@ -1,9 +1,18 @@
-import {useContext, useEffect} from 'react';
+import {useContext, useEffect, useRef} from 'react';
 import {RouteContext} from './RouteContext';
 import type {NavigationHandler} from './types/NavigationHandler';
 
 export function useNavigationStart(callback: NavigationHandler) {
+    let initedRef = useRef(false);
     let route = useContext(RouteContext);
 
-    useEffect(() => route.use(callback), [route, callback]);
+    useEffect(() => {
+        if (!initedRef.current && route.initialized) {
+            callback(route.href, '');
+
+            initedRef.current = true;
+        }
+
+        return route.use(callback);
+    }, [route, callback]);
 }
